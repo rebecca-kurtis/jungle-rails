c579e1caa004f2ca1084466be732c84328b4d101
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-04-16 19:17:28 -0700

    Add updated code for admin dashboard page/counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
   
   def show
+    @count_products = Product.count
+    @count_categories = Category.count
   end
+
 end
